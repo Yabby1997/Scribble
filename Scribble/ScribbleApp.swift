f8e6e8bcedb3bb28055138626434b7e2383ecf64
yabby1997 <yabby1997@gmail.com>
2025-10-14 20:06:06 +0900

    Initial Commit

---
+++ Scribble/ScribbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScribbleApp.swift
+//  Scribble
+//
+//  Created by Seunghun on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ScribbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
